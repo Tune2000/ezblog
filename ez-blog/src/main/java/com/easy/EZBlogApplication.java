eb9ebac25c034dfd0014cda8f1c449de42157997
Tune <11282429+Tune2000@user.noreply.gitee.com>
2023-09-01 15:05:46 +0800

    完成自动更新文章浏览量次数的需求

--- ez-blog/src/main/java/com/easy/EZBlogApplication.java
+++ ez-blog/src/main/java/com/easy/EZBlogApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @author wxx
@@ -13,6 +14,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
  */
 @SpringBootApplication
 @MapperScan("com.easy.mapper")
+@EnableScheduling
 public class EZBlogApplication {
     public static void main(String[] args) {
         SpringApplication.run(EZBlogApplication.class,args);
