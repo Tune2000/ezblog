c47d02cd903504acd1de6940ce845456bbbec501
Tune2000 <11282429+Tune2000@user.noreply.gitee.com>
2023-12-27 17:47:16 +0800

    添加README、上传数据库sql和application.yml配置修改

--- ez-blog/src/main/java/com/easy/job/UpdateViewCountJob.java
+++ ez-blog/src/main/java/com/easy/job/UpdateViewCountJob.java
@@ -3,22 +3,33 @@ package com.easy.job;
 import com.easy.domain.entity.Article;
 import com.easy.service.ArticleService;
 import com.easy.utils.RedisCache;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalTime;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 @Component
-public class UpdateViewCountJob {
+public class UpdateViewCountJob implements ApplicationContextAware {
 
     @Autowired
     private RedisCache redisCache;
 
     @Autowired
     private ArticleService articleService;
+
+    private static ApplicationContext context;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        context = applicationContext;
+    }
 
     @Scheduled(cron = "0/60 * * * * ?")
     //定时同步Redis中的数据到数据库
@@ -32,6 +43,8 @@ public class UpdateViewCountJob {
                 .collect(Collectors.toList());
         //更新到数据库中
         articleService.updateBatchById(articles);
-
+        System.out.println("redis的文章浏览量数据已更新到数据库，现在的时间是: "+ LocalTime.now());
     }
+
+
 }
